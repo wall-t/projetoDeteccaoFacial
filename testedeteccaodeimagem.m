0825fc3f53eecd7f3facf4ae686644503c0644f7
joaoaluz <joao_aluz@hotmail.com>
2016-09-05 23:54:22 -0300

    Test
    test

---
+++ testedeteccaodeimagem.m
@@ -0,0 +1,20 @@
+clear all
+clc
+
+
+%To detect Face
+FDetect = vision.CascadeObjectDetector;
+
+%Read the input image
+I = imread('C:\Users\joao__000\Pictures\fotos\im.jpg');
+%Returns Bounding Box values based on number of objects
+BB = step(FDetect,I);
+
+figure,
+imshow(I); hold on
+for i = 1:size(BB,1)
+    rectangle('Position',BB(i,:),'LineWidth',5,'LineStyle','-','EdgeColor','r');
+end
+title('Detecção facial');
+hold off;
+
