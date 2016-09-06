ed8e0209878bb5460c1971fed4862dd6c4407177
wall-t <wall-t@users.noreply.github.com>
2016-09-05 23:05:53 -0300

    Update testedeteccaodeimagem.m

--- testedeteccaodeimagem.m
+++ testedeteccaodeimagem.m
@@ -15,6 +15,6 @@ imshow(I); hold on
 for i = 1:size(BB,1)
     rectangle('Position',BB(i,:),'LineWidth',5,'LineStyle','-','EdgeColor','r');
 end
-title('Detecção Facial');
+title(' Facial Detection');
 hold off;
 
